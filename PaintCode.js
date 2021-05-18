1d39c81b995e4a249300fe9be2dadc844781e28d
Gabriel Custodio <gkustodor@gmail.com>
2021-05-18 16:57:11 -0300

    Preparing to fix code.

--- PaintCode.js
+++ PaintCode.js
@@ -5,50 +5,49 @@ var radius = 50;
 var isPainting = false;
 
 
-    function setWidth (value) {
-        if (isNumeric(value)){
-            paintcanvas.width = value;
-        }
-        else{
-            alert ("Insert a valid number!")
-        }
+function setWidth (value) {
+    if (isNumeric(value)){
+        paintcanvas.width = value;
     }
-
-    function setHeight(value) {
-        if (isNumeric(value)){
-            paintcanvas.height = value;
-        }
-        else{
-            alert ("Insert a valid number!")
-        }
+    else{
+        alert ("Insert a valid number!");
     }
+}
 
-    function startPaint(){
-        isPainting = true;
-    }    
-
-    function endPaint(){
-        isPainting = false;
+function setHeight(value) {
+    if (isNumeric(value)){
+        paintcanvas.height = value;
     }
+    else{
+        alert ("Insert a valid number!");
+    }
+}
 
-    function clearCanvas () {
+function startPaint(){
+    isPainting = true;
+}    
+
+function endPaint(){
+    isPainting = false;
+}
+
+function clearCanvas () {
     context.clearRect(0, 0, paintcanvas.width, paintcanvas.height);
-    }
+}
 
-    function paintCircle (x, y) {
-        context.beginPath();
-        context.arc(x, y, radius, 0, Math.PI * 2, true);
-        context.fillStyle = color;
-        context.fill();
-    }
+function paintCircle (x, y) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, Math.PI * 2, true);
+    context.fillStyle = color;
+    context.fill();
+}
 
-    function doPaint(x,y){
-        if(isPainting){
-            paintCircle(x,y);
-        }
-        
-    }
+function doPaint(x,y){
+    if(isPainting){
+        paintCircle(x,y);
+    }   
+}
 
-    function isNumeric (value) {
-    return !isNaN(value);
-    }
+function isNumeric (value) {
+return !isNaN(value);
+}
